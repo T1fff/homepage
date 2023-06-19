f4af0a10a90b42ea65517330c32b23b7b3b7f827
T1fff <tm250604@gmail.com>
2023-06-19 00:07:51 -0500

    Added my projects section and animations

---
+++ index.js
@@ -0,0 +1,4 @@
+gsap.from("nav", {opacity: 0, duration: 2, delay: 1.5, y:25})
+gsap.from(".primary", {opacity: 0, duration: 2, delay: 2, y:25})
+gsap.from(".my-projects", {opacity: 0, duration: 2, delay: 2.5, y:40})
+gsap.from(".secondary", {opacity: 0, duration: 2, delay: 5, y:40})
